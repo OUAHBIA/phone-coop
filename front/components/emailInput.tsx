df013b5f2b6df9261e7e63e4819ca4177523e4bb
ouahbi <ouahbi@MacBook-Air-de-ouahbi-2.local>
2021-11-30 16:58:35 +0100

    feat(front):last change on the input

--- front/components/emailInput.tsx
+++ front/components/emailInput.tsx
@@ -1,6 +1,6 @@
 import styles from "../components/Input.module.css";
 
-export default function emailInput({children,}: {children: React.ReactNode;}) {
+export default function emailInput() {
   return <input type="text" id="email" className="{style.input}" >Entrer your email</input>
 }
 
